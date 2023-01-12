2033fec77a9b91bbbaa45cfc54e6cf0f1d66720f
ULUL <ulul0.0lee@gmail.com>
2023-01-13 00:08:13 +0900

    docs: Make it simple

---
+++ 001.c
@@ -0,0 +1,13 @@
+/*************************
+ * 파일 이름 : 001.c
+ * 설명 : Simple Operations
+ * 작성자 : 이유엘
+ *************************/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("2+3=%d\n",2+3);
+    return 0;
+}
